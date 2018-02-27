41eb582d4e17e265f1eb8e52ce6090b92ed70ae8
CoderSango <CoderSango@users.noreply.github.com>
2018-02-27 16:51:02 +0000

    Add files via upload

---
+++ env.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "PAGE_ACCESS_TOKEN": "EAACiu6O47YcBAEo4OMTCQYDgDXSywHkyvBqQBwCCuy9XAwCjniBncVfSNJtjt91M9tbhJR1fz0NTqJM1PbnoGtLLvCVSrLfHkwt9LznFmNmoR8lt3o8sOZCDfzhO1ZCxBsHBAMKuJnFAY4igZBbavMn0ony5cFY5A4kELrAE7FfX5YSJPK9",  
+    "VERIFY TOKEN": "legion_are_we_the_dispossessed"
+  }  
